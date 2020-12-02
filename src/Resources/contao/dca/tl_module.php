d1824dbe1d7ca385c65f736f6a958faaad03955a
Andreas Schempp <andreas.schempp@terminal42.ch>
2020-12-02 13:43:02 +0100

    Update tl_module.php

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -15,7 +15,7 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['styleManager'] = array
     'sql'                     => "blob NULL"
 );
 
-$GLOBALS['TL_DCA']['tl_module']['fields']['cssID']['sql'] = "text NOT NULL default ''";
+$GLOBALS['TL_DCA']['tl_module']['fields']['cssID']['sql'] = "text NULL";
 
 $GLOBALS['TL_DCA']['tl_module']['config']['onload_callback'][] = array('\\Oveleon\\ContaoComponentStyleManager\\StyleManager', 'addPalette');
 $GLOBALS['TL_DCA']['tl_module']['fields']['cssID']['load_callback'][] = array('\\Oveleon\\ContaoComponentStyleManager\\StyleManager', 'onLoad');
