687e24f54d7c9b7ab4eeeebe5509a47d36192e4a
fritzmg <fmg@inspiredminds.at>
2022-03-02 14:28:01 +0000

    fix undefined array key warning

--- src/Resources/contao/dca/tl_style_manager_archive.php
+++ src/Resources/contao/dca/tl_style_manager_archive.php
@@ -91,7 +91,7 @@ $GLOBALS['TL_DCA']['tl_style_manager_archive'] = array
                 'label'               => &$GLOBALS['TL_LANG']['tl_style_manager_archive']['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.svg',
-                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+                'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
             ),
             'show' => array
             (
