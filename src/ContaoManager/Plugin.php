b19e8b215ccb2da2289d03ddf691b0ff340b2994
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-08-02 12:46:07 +0200

    Register routes directly

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -36,8 +36,8 @@ class Plugin implements BundlePluginInterface, RoutingPluginInterface
     public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel): RouteCollection|null
     {
         return $resolver
-            ->resolve(__DIR__.'/../../config/routes.yaml')
-            ->load(__DIR__.'/../../config/routes.yaml')
+            ->resolve('@ContaoComponentStyleManager/src/Controller')
+            ->load('@ContaoComponentStyleManager/src/Controller')
         ;
     }
 }
