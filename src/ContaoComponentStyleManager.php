d42445b5396c7ed0884dfe349d9e1d7c95d46589
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-10-22 15:14:01 +0200

    Extend from the AbstractBundle instead

--- src/ContaoComponentStyleManager.php
+++ src/ContaoComponentStyleManager.php
@@ -10,12 +10,36 @@ declare(strict_types=1);
 
 namespace Oveleon\ContaoComponentStyleManager;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class ContaoComponentStyleManager extends Bundle
+class ContaoComponentStyleManager extends AbstractBundle
 {
-    public function getPath(): string
+    public function configure(DefinitionConfigurator $definition): void
     {
-        return \dirname(__DIR__);
+        $definition->rootNode()
+            ->children()
+                ->booleanNode('use_bundle_config')
+                    ->defaultTrue()
+                ->end()
+                ->booleanNode('strict')
+                    ->defaultFalse()
+                ->end()
+                ->booleanNode('show_group_title')
+                    ->defaultTrue()
+                ->end()
+            ->end()
+        ;
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+
+        $builder->setParameter('contao_component_style_manager.use_bundle_config', $config['use_bundle_config']);
+        $builder->setParameter('contao_component_style_manager.strict', $config['strict']);
+        $builder->setParameter('contao_component_style_manager.show_group_title', $config['show_group_title']);
     }
 }
