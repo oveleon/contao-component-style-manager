d2816f3f3c3f79238d4fc3fb6db0cd58d533de31
doishub <daniele@oveleon.de>
2026-04-07 12:57:37 +0200

    Contao 5.7

--- src/StyleManager/StyleManager.php
+++ src/StyleManager/StyleManager.php
@@ -415,7 +415,7 @@ class StyleManager
     }
 
     #[AsCallback(table: 'tl_form_field', target: 'list.sorting.child_record')]
-    public function listFormFields(array $arrRow): string|array
+    public function listFormFields(array $arrRow): string
     {
         $arrStyles = StringUtil::deserialize($arrRow['styleManager']);
         $arrRow['styleManager'] = new Styles($arrStyles[StyleManager::VARS_KEY] ?? null);
